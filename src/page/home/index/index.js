cf96cb75eb99b9e1b9f32f9ae3185fc3619145a5
xugang <371801080@qq.com>
2018-06-06 23:47:32 +0800

    首页 和个人中心静态页面

--- src/page/home/index/index.js
+++ src/page/home/index/index.js
@@ -11,7 +11,36 @@ Page({
 		advs: ['https://m.360buyimg.com/babel/jfs/t20695/118/711678007/159042/81b387b2/5b157234Na31baa10.png',
 			'https://m.360buyimg.com/babel/jfs/t20887/304/737271706/39873/327236a2/5b166eb7Nba515084.png',
 			'https://m.360buyimg.com/babel/jfs/t22222/31/459858983/48865/8768dca/5b0e5419N0855116e.jpg',
-			'https://m.360buyimg.com/babel/jfs/t21301/283/505859890/95478/7f6a9596/5b0f9953Nd4ac7ccc.jpg']
+			'https://m.360buyimg.com/babel/jfs/t21301/283/505859890/95478/7f6a9596/5b0f9953Nd4ac7ccc.jpg'
+		],
+		noticeList: [{
+			id: 1,
+			content: '1、2018年5月18日，图书馆一楼会议室在上午08:00- 12:00，暂停对外开放，请各位注意调整预约。'
+		},{
+			id: 2,
+			content: '2、2018年5月19日，图书馆因检查更换设备，暂停对外开放使用，请各位注意调整预约时间。'
+		}],
+		orderList: [{
+			id: 1,
+			content: '图书馆一楼A区阅读区',
+			time: "2018/5/18 09:00-13:00",
+			status: 0
+		},{
+			id: 1,
+			content: '图书馆三楼C区多功能区',
+			time: "2018/5/18 09:00-13:00",
+			status: 1
+		},{
+			id: 1,
+			content: '图书馆二楼D区多功能区 ',
+			time: "2018/5/18 09:00-13:00",
+			status: 0
+		},{
+			id: 1,
+			content: '图书馆二楼D区多功能区',
+			time: "2018/5/18 09:00-13:00",
+			status: 0
+		}]
 	},
 	
 	onLoad: function(){
--- src/page/user/index/index.js
+++ src/page/user/index/index.js
@@ -1,10 +1,17 @@
 // import { getUserInfo } from '../../resource/utils/comment.js'
 Page({
     data: {
-
+		info: {},
+		tagList: ['看书','旅行','唱歌','做甜点']
     },
 
     onLoad: function (options) {
-		
+		this.setData({
+			info: {
+				name: '许刚',
+				count: 13797071376,
+				headImg: 'http://xxb.xuexunbao.com.cn/201806062227018863cfc5b771da58f0ae5acd19064476b22.jpg'
+			}
+		})
     }
 })
