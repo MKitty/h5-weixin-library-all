a2c3cef9e6ec170fe105d5df128f88c056edc50c
xugang <371801080@qq.com>
2018-06-09 22:30:49 +0800

    静态页面

--- src/page/order/index/index.js
+++ src/page/order/index/index.js
@@ -1,4 +1,3 @@
-// import { getUserInfo } from '../../resource/utils/comment.js'
 Page({
     data: {
 
@@ -6,5 +5,15 @@ Page({
 
     onLoad: function (options) {
 		
+    },
+
+    goToDetail(e){
+		let type = e.currentTarget.dataset.type;
+		let status = e.currentTarget.dataset.status;
+
+		wx.navigateTo({
+	        url: status == 1 ?  `/src/page/order/listAll/index?type=${type}&status=${status}`
+	        				 :  `/src/page/order/list/index?type=${type}&status=${status}`
+    	})
     }
 })
---
+++ src/page/order/listAll/index.js
@@ -0,0 +1,17 @@
+// import { getUserInfo } from '../../resource/utils/comment.js'
+Page({
+    data: {
+
+    },
+
+    onLoad: function (options) {
+    	let type = options.type,
+    		status = options.status;
+		
+		
+		// wx.setNavigationBarTitle({
+  //           title: '详情'
+  //       })
+
+    }
+})
